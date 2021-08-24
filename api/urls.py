3785275d0d5ff8e5cf3a151850db73b149fd4bed
omar115 <omarhasan115@gmail.com>
2021-08-25 00:07:25 +0600

    complete crud opetions done using class view api

--- api/urls.py
+++ api/urls.py
@@ -3,5 +3,10 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('status/', StatusAPIView.as_view()),
+    path('status/v1/', StatusAPIView.as_view()),
+    path('status/v2/', StatusListApiView.as_view()),    # to see all status list
+    path('status/create/', StatusCreateApiView.as_view()),  # to create a new status
+    path('status/v2/detail/<id>/', StatusDetailApiView.as_view()),  # to see single status detail
+    path('status/v2/detail/update/<id>/', StatusUpdateApiView.as_view()),   # to update one status
+    path('status/v2/delete/<id>/', StatusDeleteApiView.as_view()),  # to delete a single status
 ]
--- api/views.py
+++ api/views.py
@@ -1,8 +1,10 @@
+from django.db.models import manager
 from django.shortcuts import render
 from .models import *
 from .serializers import *
 
 from rest_framework.views import APIView
+from rest_framework.generics import ListAPIView, CreateAPIView, RetrieveAPIView, UpdateAPIView, DestroyAPIView
 from rest_framework.response import Response
 
 
@@ -15,3 +17,37 @@ class StatusAPIView(APIView):
         status_list = Status.objects.all()
         status_serializer = StatusSerializer(status_list, many=True)
         return Response(status_serializer.data)
+
+
+# In APIView we need to specify the get method, but in ListAPIView, we do not need to specify the 
+# get method, we just need to put the queryset (means you need to mention the model name that you  need to query)
+# and serializer_class (means you need to mention the serializer name to serialize the data)
+class StatusListApiView(ListAPIView):
+    queryset = Status.objects.all()
+    serializer_class = StatusSerializer
+
+
+class StatusCreateApiView(CreateAPIView):
+    queryset = Status.objects.all()
+    serializer_class = StatusSerializer
+
+# RetrieveAPIView will give you access to see single status by searching with unique entry
+class StatusDetailApiView(RetrieveAPIView):
+    queryset = Status.objects.all()
+    serializer_class = StatusSerializer
+    lookup_field = 'id'
+
+# difference between PUT and PATCH --> PUT: you have to send the full object to update one entry
+# PATCH: you just need to send the updated entry, not full object
+# UpdateAPIView will support both PUT and PATCH but not GET method
+
+class StatusUpdateApiView(UpdateAPIView):
+    queryset = Status.objects.all()
+    serializer_class = StatusSerializer
+    lookup_field = 'id'
+
+
+class StatusDeleteApiView(DestroyAPIView):
+    queryset = Status.objects.all()
+    serializer_class = StatusSerializer
+    lookup_field = 'id'
